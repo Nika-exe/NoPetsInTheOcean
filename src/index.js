e91f6048af7220f6e8115f332ae7918053ce396a
Carlos García <carlos.garciamor@nube.unadmexico.mx>
2022-05-14 21:40:19 -0500

    Responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+let burguerMenu = document.querySelector(".burguerFlex")
+burguerMenu.addEventListener("click", crossLine)
+
 function crossLine() {
   this.classList.toggle("menu");
 }
-let burguerMenu = document.querySelector(".burguerFlex");
-
-burguerMenu.addEventListener("click", crossLine);
---
+++ src/templates.js
@@ -0,0 +1,23 @@
+let headerTag = document.getElementById('beach-header')
+let footerTag = document.getElementById('beach-footer')
+
+callTemplates();
+
+async function callTemplates() {
+  let templates = document.createElement('template')
+  templates.innerHTML = await (
+    await fetch('../public/templates.html')).text()
+
+  let headerTemplate = templates.content.getElementById( 'beach-header' )
+  let footerTemplate = templates.content.getElementById( 'beach-footer' )
+  
+  headerTag.innerHTML = headerTemplate.innerHTML
+  footerTag.innerHTML = footerTemplate.innerHTML
+
+  console.log(headerTag.innerHTML)
+
+}
+
+
+
+
