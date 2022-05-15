1e73a44757b3c3a59b50d31d8ec59351eb858441
Carlos García <carlos.garciamor@nube.unadmexico.mx>
2022-05-15 03:46:34 -0500

    Header&footer

--- src/index.js
+++
@@ -1,6 +0,0 @@
-let burguerMenu = document.querySelector(".burguerFlex")
-burguerMenu.addEventListener("click", crossLine)
-
-function crossLine() {
-  this.classList.toggle("menu");
-}
--- src/templates.js
+++ src/templates.js
@@ -1,22 +1,29 @@
-let headerTag = document.getElementById('beach-header')
-let footerTag = document.getElementById('beach-footer')
+let burguerMenu = document.querySelector(".burguerFlex")
+burguerMenu.addEventListener("click", crossLine)
 
-callTemplates();
-
-async function callTemplates() {
-  let templates = document.createElement('template')
-  templates.innerHTML = await (
-    await fetch('../public/templates.html')).text()
-
-  let headerTemplate = templates.content.getElementById( 'beach-header' )
-  let footerTemplate = templates.content.getElementById( 'beach-footer' )
-  
-  headerTag.innerHTML = headerTemplate.innerHTML
-  footerTag.innerHTML = footerTemplate.innerHTML
-
-  console.log(headerTag.innerHTML)
-
+function crossLine() {
+    this.classList.toggle("menu");
 }
+
+// let headerTag = document.getElementById('beach-header')
+// let footerTag = document.getElementById('beach-footer')
+
+// callTemplates();
+
+// async function callTemplates() {
+//   let templates = document.createElement('template')
+//   templates.innerHTML = await (
+//     await fetch('../public/templates.html')).text()
+
+//   let headerTemplate = templates.content.getElementById( 'beach-header' )
+//   let footerTemplate = templates.content.getElementById( 'beach-footer' )
+  
+//   headerTag.innerHTML = headerTemplate.innerHTML
+//   footerTag.innerHTML = footerTemplate.innerHTML
+
+//   console.log(headerTag.innerHTML)
+
+// }
 
 
 
