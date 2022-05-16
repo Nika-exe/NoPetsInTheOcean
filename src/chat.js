105df2273a4c8d2348d0cdceb26eaab819027ac6
MirIstKalt4 <sarquisben@hotmail.com>
2022-05-15 19:27:46 -0500

    add: feat-chat-front

---
+++ src/chat.js
@@ -0,0 +1,28 @@
+(function (window, document, JSON) {
+    'use strict';
+    let url = 'ws://' + window.location.host + '/chat_war/chat',
+        ws = new WebSocket(url),
+        chat = document.getElementById('chat'),
+        btn = document.getElementById('btnSend'),
+        nombre = document.getElementById('nombre'),
+        mensaje = document.getElementById('mensaje');
+
+    ws.onopen = () => console.log('conectado...');
+    ws.onclose = () => console.log('desconectado...');
+    ws.onmessage = (e) => {
+        let data = JSON.parse(e.data);
+        console.log(data);
+        chat.innerHTML += `<br>Nombre: ${data.nombre} dice: ${data.mensaje}`
+    };
+
+    btn.addEventListener('click', () => {
+        let msg = {
+            nombre: nombre.value,
+            mensaje: mensaje.value
+        };
+
+        console.log(msg);
+
+        ws.send(JSON.stringify(msg));
+    });
+})(window, document, JSON);
