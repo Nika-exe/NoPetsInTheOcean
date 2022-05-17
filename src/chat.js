eeada0e9d53af8f5699c7e248cdc11311c33e9a0
MirIstKalt4 <sarquisben@hotmail.com>
2022-05-17 02:15:04 -0500

    refactor: Change project files organization

--- src/chat.js
+++ src/chat.js
@@ -2,17 +2,33 @@
     'use strict';
     let url = 'ws://40.122.237.236:8080/chat',
         ws = new WebSocket(url),
+        today = new Date(),
         chat = document.getElementById('chat'),
         btn = document.getElementById('btnSend'),
         nombre = document.getElementById('nombre'),
-        mensaje = document.getElementById('msg');
+        mensaje = document.getElementById('mensaje');
 
-    ws.onopen = () => console.log('conectado...');
+    const audio = document.createElement('audio');
+    audio.setAttribute('src','assets/mp3/sound.mp3');
+
+    ws.onopen = () => {
+        console.log('conectado...');
+        if(localStorage.chat) {
+            chat.value = localStorage.chat;
+        }
+    };
     ws.onclose = () => console.log('desconectado...');
     ws.onmessage = (e) => {
         let data = JSON.parse(e.data);
-        console.log(data);
-        chat.innerHTML += `<br>Nombre: ${data.nombre} dice: ${data.mensaje}`
+        const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        chat.value += `[${time}] ${data.nombre} :\n${data.mensaje}\n`
+        chat.scrollTop = chat.scrollHeight;
+        if(data.nombre !== nombre.value) {
+            audio.pause();
+            audio.currentTime = 0;
+            audio.play();
+        }
+        localStorage.setItem('chat', chat.value);
     };
 
     btn.addEventListener('click', (e) => {
@@ -22,8 +38,10 @@
             mensaje: mensaje.value
         };
 
-        console.log(msg);
+        if(msg.nombre !== '' && msg.mensaje !== '') {
+            ws.send(JSON.stringify(msg));
+        }
 
-        ws.send(JSON.stringify(msg));
+        mensaje.value = '';
     });
 })(window, document, JSON);
